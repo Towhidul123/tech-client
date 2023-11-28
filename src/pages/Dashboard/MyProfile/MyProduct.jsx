337ba62e90e41edbb8ac423ea2a5a58b1cb40286
Towhidul123 <excaliburfsh@gmail.com>
2023-11-29 02:17:34 +0600

    delete operation on user product

--- src/pages/Dashboard/MyProfile/MyProduct.jsx
+++ src/pages/Dashboard/MyProfile/MyProduct.jsx
@@ -1,8 +1,43 @@
 import React, { useEffect, useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const MyProduct = () => {
 
     const [card, setCard] = useState([]);
+    const handleDelete = id =>{
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/dashboard/userProduct/${id}`, {
+                    method: 'DELETE'
+                })
+                .then((res) => res.json())
+                    .then((data) => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire('Deleted!', 'Your product has been deleted.', 'success').then(() => {
+                              
+                                window.location.reload();
+                            });
+                        }
+                    })
+                    .catch((error) => {
+                        console.error('Error deleting product:', error);
+                     
+                    });
+
+            }
+          });
+    }
+
 
     useEffect(() => {
         fetch('http://localhost:5000/dashboard/userProduct')
@@ -22,7 +57,7 @@ const MyProduct = () => {
                     <thead>
                         <tr>
                             <th>
-                                
+                                    #
                             </th>
                             <th>Name</th>
                             <th>No. of votes</th>
@@ -31,44 +66,40 @@ const MyProduct = () => {
                         </tr>
                     </thead>
                     <tbody>
-                       
-                       {
-                        card.map(item => <tr key={item._id}>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
-                            <td>
-                                <div className="flex items-center gap-3">
-                                    <div className="avatar">
-                                        <div className="mask mask-squircle w-12 h-12">
-                                            <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+
+                        {
+                            card.map((item, index) => <tr key={item._id}>
+                                <th>
+                                    {index + 1}
+                                </th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={item.productImage} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">{item.productName}</div>
+
                                         </div>
                                     </div>
-                                    <div>
-                                        <div className="font-bold">Hart Hagerty</div>
-                                        <div className="text-sm opacity-50">United States</div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                Zemlak, Daniel and Leannon
-                                <br />
-                                <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-                            </td>
-                            <td>Purple</td>
-                            <th>
-                                <button className="btn btn-ghost btn-xs">details</button>
-                            </th>
-                        </tr>)
-                       }
+                                </td>
+                                <td>
+                                    {item.productVote}
+                                </td>
+                                    {item.status}
+                                <th>
+                                    <button onClick={() =>handleDelete(item._id)} className="btn btn-ghost btn-lg"><FaTrashAlt className='text-red-600'></FaTrashAlt></button>
+                                </th>
+                            </tr>)
+                        }
 
 
-                        
-                       
+
+
                     </tbody>
-                    
+
 
                 </table>
             </div>
